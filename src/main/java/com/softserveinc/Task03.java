fcd4f3167659eebbda04b96875cbb19501c44c09
oteplov <oteplov@softserveinc.com>
2022-01-14 01:29:32 +0200

    HW#2 Task#3

--- src/main/java/com/softserveinc/Task03.java
+++ src/main/java/com/softserveinc/Task03.java
@@ -1,6 +1,7 @@
 package com.softserveinc;
 
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /*
     Write a program that determines the minimum of a sequence positive numbers (the length of the sequence is not limited).
@@ -18,11 +19,32 @@ import java.util.Scanner;
     >>> 23
     >>> 0
     Minimum number is 9
+
+            for (int i = 0; i < listOfNumbers.size(); i++) {
+            listOfNumbers
+        }
  */
 public class Task03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // TODO: Write your code here
+        List<Integer> fullListOfNumbers = new ArrayList<>();
+        int enteredValue;
+        int min;
+        do {
+            System.out.print(">>> ");
+            enteredValue = scanner.nextInt();
+            fullListOfNumbers.add(enteredValue);
+        } while (enteredValue != 0);
 
+        for (int i=0; i < fullListOfNumbers.size(); i++){
+            if (fullListOfNumbers.get(i).equals(0)){
+                fullListOfNumbers.remove(i);
+            }
+        }
+
+        Collections.sort(fullListOfNumbers);
+        min = fullListOfNumbers.get(0);
+        System.out.println("Minimum number is " + min);
     }
 }
